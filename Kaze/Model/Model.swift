14dc268948d3322173b053278c5b557a28a11a86
HikaruTakahashi <hikaru@hikaru-takahashi-no-MacBook.local>
2016-07-05 22:14:13 +0900

    add MVVM

---
+++ Kaze/Model/Model.swift
@@ -0,0 +1,11 @@
+//
+//  Model.swift
+//  Kaze
+//
+//  Created by HikaruTakahashi on 2016/07/05.
+//  Copyright © 2016年 HikaruTakahashi. All rights reserved.
+//
+
+class Model {
+
+}
---
+++ Kaze/ViewModel/ViewModel.swift
@@ -0,0 +1,11 @@
+//
+//  ViewModel.swift
+//  Kaze
+//
+//  Created by HikaruTakahashi on 2016/07/05.
+//  Copyright © 2016年 HikaruTakahashi. All rights reserved.
+//
+
+class ViewModel {
+
+}
